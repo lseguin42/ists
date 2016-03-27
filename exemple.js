3f9710ff6789eefa2ea0490c1e9ff1d5f976925b
Lionel Seguin <lseguin@student.42.fr>
2016-03-28 00:53:15 +0200

    add exemple

--- exemple.js
+++ exemple.js
@@ -1,8 +1,17 @@
 var _ = require('lodash');
 var is = require('./isjs');
 
-var rules = is.number.in.list(67, 34, 21, 43).and.not.in.list(15, 85, 21, 78, 43)
+
+var _1 = is.number.equal(6);
+
+var _2 = is.number.inf(-5).or.sup(50);
+
+var _3 = is.number.not.in.range(5, 50).or(is.string);
+
+var _4 = is.number.in.list(67, 34, 21, 43).and.not.in.list(15, 85, 21, 78, 43)
                .or(is.string.in.list("hello", "miaou", "toto").or.match(/^n/))
+
+
 
 var exemples = [
     "natoo",
@@ -12,13 +21,20 @@ var exemples = [
     313,
     67,
     34,
+    6,
     21,
     43,
+    -1,
+    -9,
     "Nuloss",
     "norbert"
 ];
 
-_.forEach(exemples, function(elem) {
-    console.log(elem + "\t[" + rules(elem) + "]");
+var d = function (b) {
+    return b ? 'x' : ' ';
+}
+
+_.forEach(exemples, function(e) {
+    console.log(e + "\t[" + d(_1(e)) + "]\t[" + d(_2(e)) + "]\t[" + d(_3(e)) + "]\t[" + d(_4(e)) + "]");
 })
 
