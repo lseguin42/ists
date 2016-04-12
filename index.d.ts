ea5d3539fddedf68c368f9b93dff3d3603a4d11e
Lionel Seguin <Lionel@iMac-de-Lionel.local>
2016-04-12 20:32:38 +0200

    Fixed bug

--- index.d.ts
+++ index.d.ts
@@ -1,138 +1,140 @@
-interface Operator {
-    (value: IRules): Rules<Operator>;
-}
+declare var is: IsTS.IsChecker
 
-type OperatorTemplate<T> = Operator & T;
+declare module IsTS {
+    interface Operator {
+        (value: IRules): Rules<Operator>;
+    }
 
-interface IRules {
-    (value: any): boolean;
-}
+    type OperatorTemplate<T> = Operator & T;
 
-interface _Rules<T> extends IRules{
-   
-    or: OperatorTemplate<T>;
+    interface IRules {
+        (value: any): boolean;
+    }
+
+    interface _Rules<T> extends IRules{
     
-    and: OperatorTemplate<T>;
-    
-}
+        or: OperatorTemplate<T>;
+        
+        and: OperatorTemplate<T>;
+        
+    }
 
-type Rules<T> = _Rules<T> & T
+    type Rules<T> = _Rules<T> & T
 
-interface IsNumberTemplate<T> extends
-    Not<IsNumberTemplate<T>>,
-    InExtends<number, T>
-{   
-    equal(value: number): Rules<T>;
-    inf(value: number): Rules<T>;
-    sup(value: number): Rules<T>;
-    supeq(value: number): Rules<T>;
-    infeq(value: number): Rules<T>;
-}
+    interface IsNumberTemplate<T> extends
+        Not<IsNumberTemplate<T>>,
+        InExtends<number, T>
+    {   
+        equal(value: number): Rules<T>;
+        inf(value: number): Rules<T>;
+        sup(value: number): Rules<T>;
+        supeq(value: number): Rules<T>;
+        infeq(value: number): Rules<T>;
+    }
 
-interface IsNumber extends IsNumberTemplate<IsNumber> {}
+    interface IsNumber extends IsNumberTemplate<IsNumber> {}
 
-interface Not<T> {
-    not: T
-}
+    interface Not<T> {
+        not: T
+    }
 
-interface Length<T> {
-    length: IsNumberTemplate<T>
-}
+    interface Length<T> {
+        length: IsNumberTemplate<T>
+    }
 
-interface InContainer<ElementType, T> {
-    array(elements: ElementType[]): Rules<T>
-    list(...args: ElementType[]): Rules<T>
-}
+    interface InContainer<ElementType, T> {
+        array(elements: ElementType[]): Rules<T>
+        list(...args: ElementType[]): Rules<T>
+    }
 
-interface InContainerExtends<ElementType, T> extends InContainer<ElementType, T> {
-    range(min: ElementType, max: ElementType)
-}
+    interface InContainerExtends<ElementType, T> extends InContainer<ElementType, T> {
+        range(min: ElementType, max: ElementType)
+    }
 
-interface In<ElementType, T> {
-    in: InContainer<ElementType, T>
-}
+    interface In<ElementType, T> {
+        in: InContainer<ElementType, T>
+    }
 
-interface InExtends<ElementType, T> {
-    in: InContainerExtends<ElementType, T>
-}
+    interface InExtends<ElementType, T> {
+        in: InContainerExtends<ElementType, T>
+    }
 
-interface IsString extends
-    Not<IsString>,
-    In<string, IsString>,
-    Length<IsString>
-{
-    equal(value: string): Rules<IsString>;
-    match(regex: RegExp): Rules<IsString>;
-}
+    interface IsString extends
+        Not<IsString>,
+        In<string, IsString>,
+        Length<IsString>
+    {
+        equal(value: string): Rules<IsString>;
+        match(regex: RegExp): Rules<IsString>;
+    }
 
-interface With<T> {
-    with: {
-        properties(object: any): Rules<T>
-        property(name: string, rule: IRules): Rules<T>
+    interface With<T> {
+        with: {
+            properties(object: any): Rules<T>
+            property(name: string, rule: IRules): Rules<T>
+        }
+    }
+
+    interface IsObject extends 
+        Not<IsObject>,
+        With<IsObject>
+    {
+        instanceOf(type: any): Rules<IsObject>
+    }
+
+    interface IsArray extends
+        Not<IsArray>,
+        Length<IsArray>
+    {
+        each(rule: IRules, options?: { start: number, end: number }): Rules<IsArray>
+    }
+
+    interface IsBoolean extends
+        Not<IsBoolean>
+    {
+        equal(bool: boolean): Rules<IsBoolean>
+    }
+
+    interface IsUndefined 
+    {
+    }
+
+    interface IsFunction
+    {
+    }
+
+    interface IsAny
+    {
+    }
+
+    interface IsNumberRules    extends IsNumber,    IRules {}
+    interface IsStringRules    extends IsString,    IRules {}
+    interface IsObjectRules    extends IsObject,    IRules {}
+    interface IsArrayRules     extends IsArray,     IRules {}
+    interface IsBooleanRules   extends IsBoolean,   IRules {}
+    interface IsUndefinedRules extends IsUndefined, IRules {}
+    interface IsFunctionRules  extends IsFunction,  IRules {}
+    interface IsAnyRules       extends IsAny,       IRules {}
+
+    interface IsChecker {
+        
+        number: IsNumberRules;
+        
+        string: IsStringRules;
+
+        object: IsObjectRules;
+        
+        array: IsArrayRules;
+        
+        any: IsArrayRules;
+        
+        boolean: IsBooleanRules;
+        
+        undefined: IsUndefinedRules;
+        
+        function: IsFunctionRules;
+
     }
 }
-
-interface IsObject extends 
-    Not<IsObject>,
-    With<IsObject>
-{
-    instanceOf(type: any): Rules<IsObject>
-}
-
-interface IsArray extends
-    Not<IsArray>,
-    Length<IsArray>
-{
-    each(rule: IRules, options?: { start: number, end: number }): Rules<IsArray>
-}
-
-interface IsBoolean extends
-    Not<IsBoolean>
-{
-    equal(bool: boolean): Rules<IsBoolean>
-}
-
-interface IsUndefined 
-{
-}
-
-interface IsFunction
-{
-}
-
-interface IsAny
-{
-}
-
-interface IsNumberRules    extends IsNumber,    IRules {}
-interface IsStringRules    extends IsString,    IRules {}
-interface IsObjectRules    extends IsObject,    IRules {}
-interface IsArrayRules     extends IsArray,     IRules {}
-interface IsBooleanRules   extends IsBoolean,   IRules {}
-interface IsUndefinedRules extends IsUndefined, IRules {}
-interface IsFunctionRules  extends IsFunction,  IRules {}
-interface IsAnyRules       extends IsAny,       IRules {}
-
-interface IsChecker {
-    
-    number: IsNumberRules;
-    
-    string: IsStringRules;
-
-    object: IsObjectRules;
-    
-    array: IsArrayRules;
-    
-    any: IsArrayRules;
-    
-    boolean: IsBooleanRules;
-    
-    undefined: IsUndefinedRules;
-    
-    function: IsFunctionRules;
-
-}
-
-declare var is: IsChecker
 
 export = is
